0530845127daee2c86de93d360605bcd98e962bb
a1847grace <a1847grace@gmail.com>
2021-07-16 14:20:57 +0800

    有篩選功能的版本

--- 留言分析程式.py
+++ 留言分析程式.py
@@ -1,4 +1,5 @@
 data = []
+data_100以下 = []
 count = 0
 count = bool(count)
 with open('reviews.txt', 'r') as message:
@@ -11,12 +12,16 @@ print ('檔案讀取完了，總共有：', len(data), '筆資料')
 #下列為我自行寫的程式碼
 graph = 0 
 total = 0
-total =int(total)
+total = int(total)
 graph = int(graph)
 while graph != len(data):
 	total = total + len(data[graph])
+	if len(data[graph]) < 100:
+		data_100以下.append(data[graph])
 	graph = graph + 1
 print ('我算的字串平均數為', total/len(data))
+print ('我算一百以下的字數有', len(data_100以下), '筆資料')
+print (data_100以下[0])
 
 #老師的程式碼
 sum_len = 0
@@ -24,3 +29,10 @@ sum_len =int(sum_len)
 for d in data:
 	sum_len = sum_len + len(d)
 print('老師算的平均是', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print ('老師算的一共有', len(new), '筆留言長度小於100')
+print (new[0])
